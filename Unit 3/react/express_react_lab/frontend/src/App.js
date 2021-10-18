69b1253a79dd2adf9f5a1be89a4a584ce6ef0621
hum-motor-whir <rodrigo.bucheli@gmail.com>
2021-10-18 12:28:45 -0400

    frontend complete

---
+++ Unit 3/react/express_react_lab/frontend/src/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+// IMPORT COMPONENTS
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Route, Switch } from "react-router-dom";
+// IMPORT PAGES
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+
+function App() {
+  const URL = "https://rodrigo-portfolio.herokuapp.com/";
+  return (
+    <div className="App">
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/projects">
+          <Projects URL={URL} />
+        </Route>
+        <Route path="/about">
+          <About URL={URL} />
+        </Route>
+      </Switch>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
